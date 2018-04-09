d1b33a41beeda57e4d2dbeece9868cb78149badc
Laura Johnson <laurajohnson@Laura-Johnson-MBP.local>
2018-04-09 00:08:16 -0400

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,24 @@
 
 // grab our gulp packages
 var gulp  = require('gulp'),
+    guppy = require('git-guppy')(gulp),
     gutil = require('gulp-util'),
     exec = require('child_process').exec,
     lighthouse = require('lighthouse'),
+    split = require('split-json'),
+    fs  = require('fs'),
     grok = 'http://772fbfa3.ngrok.io';
 
-gulp.task('pre-commit', ['tenon']);
+gulp.task('pre-commit', ['lighthouse']);
 
 gulp.task('tenon', function () {
 
-    //exec("curl -X POST -H Content-Type:application/x-www-form-urlencoded -H Cache-Control:no-cache -d 'url=http://772fbfa3.ngrok.io&key=2416d4080d01c2103b5c76e3b9487343' https://tenon.io/api/", function (err, stdout, stderr) {
-
-        exec("curl -X POST -H Content-Type:application/x-www-form-urlencoded -H Cache-Control:no-cache -d 'url=http://772fbfa3.ngrok.io&domain=http://772fbfa3.ngrok.io&key=2416d4080d01c2103b5c76e3b9487343&name=drupalspider&type=spider' https://tenon.io/api/projects/", function (err, stdout, stderr) {
-
+    exec("curl -X POST -H Content-Type:application/x-www-form-urlencoded -H Cache-Control:no-cache -d 'url=http://772fbfa3.ngrok.io/accessibility-tools/simple-form&key=2416d4080d01c2103b5c76e3b9487343' https://tenon.io/api/", function (err, stdout, stderr) {
 
         console.log(stderr);
 
         var response = JSON.parse(stdout);
-            gutil.log(response);
+        gutil.log(response);
 
         if (response.resultSet) {
 
@@ -32,12 +32,15 @@ gulp.task('tenon', function () {
                     for (var prop in obj) {
                         // skip loop if the property is from prototype
                         if(!obj.hasOwnProperty(prop)) continue;
-
-                        // your code
-                        gutil.log(prop + " = " + JSON.stringify(obj[prop]));
+                        if (prop == 'errorDescription' || prop == 'errorSnippet' || prop == 'resultTitle') {
+                            // your code
+                            gutil.log(prop + " = " + JSON.stringify(obj[prop]));
+                        }
                     }
                 }
             }
+            gutil.log("Please resolve these errors before committing");
+            process.exit(1);
         }
     });
 
@@ -62,13 +65,13 @@ gulp.task('wave', function () {
                 if (!response.categories.hasOwnProperty(key)) continue;
 
                 var obj = response.categories[key];
-                    for (var prop in obj) {
-                        // skip loop if the property is from prototype
-                        if(!obj.hasOwnProperty(prop)) continue;
+                for (var prop in obj) {
+                    // skip loop if the property is from prototype
+                    if(!obj.hasOwnProperty(prop)) continue;
 
-                        // your code
-                        gutil.log(prop + " = " + JSON.stringify(obj[prop]));
-                    }
+                    // your code
+                    gutil.log(prop + " = " + JSON.stringify(obj[prop]));
+                }
             }
         }
     });
@@ -78,15 +81,112 @@ gulp.task('wave', function () {
 
 gulp.task('lighthouse', function () {
 
-        exec("lighthouse http://at-build/accessibility-tools/simple-form --chrome-flags='--headless'", function (err, stdout, stderr) {
+    exec("lighthouse http://at-build/accessibility-tools/simple-form --output json --chrome-flags='--headless' --quiet", function (err, stdout, stderr) {
 
-            console.log(stderr);
+        //console.log(stderr);
 
-            var response = JSON.parse(stdout);
+        var rs = fs.readFileSync('./report.json', 'utf8');
+        var response = JSON.parse(rs);
+        //gutil.log(response.reportCategories[2].audits);
 
-            gutil.log(response);
+        for (var key in response.reportCategories[2].audits) {
+            // skip loop if the property is from prototype
+            if (!response.reportCategories[2].audits.hasOwnProperty(key)) continue;
 
-        });
+            var obj = response.reportCategories[2].audits[key];
+            var x = 0;
+            if (obj.result.details && obj.result.details.items.length > 0) {
+                x++;
+                gutil.log('-------- Lighthouse alert ' + x + ' ---------');
+                gutil.log('name:' + JSON.stringify(obj.result.name));
+                gutil.log('helpText:' + JSON.stringify(obj.result.helpText));
+                gutil.log('description:' + JSON.stringify(obj.result.description));
+                for (var it in obj.result.details.items) {
+                    var item = obj.result.details.items[it];
+                    for (var i in item) {
+                        gutil.log(i + ' = ' + item[i]);
+                    }
+                }
+                gutil.log("Please resolve these errors before committing");
+                process.exit(1);
+            }
+        }
+
+    });
+
+});
+
+gulp.task('tenonspider', function () {
+
+
+    exec("curl -d '{\"key\": \"2416d4080d01c2103b5c76e3b9487343\", \"type\": \"spider\", \"url\": \"http://772fbfa3.ngrok.io\", \"domain\": \"http://772fbfa3.ngrok.io\", \"name\": \"drupalspider3\" }' -H 'Content-Type: application/json' -X POST https://tenon.io/api/projects", function (err, stdout, stderr) {
+
+        // https://tenon.io/api/projects?key=2416d4080d01c2103b5c76e3b9487343&type=spider&url=http://772fbfa3.ngrok.io&domain=ngrok.io&name=drupalspider2
+
+        console.log(stderr);
+
+        var response = JSON.parse(stdout);
+        gutil.log(response);
+
+        if (response.resultSet) {
+
+            for (var key in response.resultSet) {
+                // skip loop if the property is from prototype
+                if (!response.resultSet.hasOwnProperty(key)) continue;
+
+                var obj = response.resultSet[key];
+                if (obj.certainty == 100) {
+                    for (var prop in obj) {
+                        // skip loop if the property is from prototype
+                        if(!obj.hasOwnProperty(prop)) continue;
+                        if (prop == 'errorDescription' || prop == 'errorSnippet' || prop == 'resultTitle') {
+                            // your code
+                            gutil.log(prop + " = " + JSON.stringify(obj[prop]));
+                        }
+                    }
+                }
+            }
+            gutil.log("Please resolve these errors before committing");
+            process.exit(1);
+        }
+    });
+
+});
+
+gulp.task('tenonapi', function () {
+
+
+    exec("curl -d '{\"key\": \"2416d4080d01c2103b5c76e3b9487343\", \"url\": \"http://772fbfa3.ngrok.io/sites/default/files/xmlsitemap/NXhscRe0440PFpI5dSznEVgmauL25KojD7u4e9aZwOM/1.xml\" }' -H 'Content-Type: application/json' -X POST https://tenon.io/api/projects/d4006c0c-e4b4-4f0e-85f7-8fb54101976b/urls/sitemap", function (err, stdout, stderr) {
+
+        // https://tenon.io/api/projects?key=2416d4080d01c2103b5c76e3b9487343&type=spider&url=http://772fbfa3.ngrok.io&domain=ngrok.io&name=drupalspider2
+
+        console.log(stderr);
+
+        var response = JSON.parse(stdout);
+        gutil.log(response);
+
+        if (response.resultSet) {
+
+            for (var key in response.resultSet) {
+                // skip loop if the property is from prototype
+                if (!response.resultSet.hasOwnProperty(key)) continue;
+
+                var obj = response.resultSet[key];
+                if (obj.certainty == 100) {
+                    for (var prop in obj) {
+                        // skip loop if the property is from prototype
+                        if(!obj.hasOwnProperty(prop)) continue;
+                        if (prop == 'errorDescription' || prop == 'errorSnippet' || prop == 'resultTitle') {
+                            // your code
+                            gutil.log(prop + " = " + JSON.stringify(obj[prop]));
+                        }
+                    }
+                }
+            }
+            gutil.log("Please resolve these errors before committing");
+            process.exit(1);
+        }
+    });
 
 });
 
